aab3c212b79d8406ae64fef6b631ed6aff88fcb1
Deborah Mayer <deborahmayer02@gmail.com>
2022-11-01 11:14:35 +0100

    Camera Version 1.0 fertig

---
+++ Assets/CALLI/Scripts/CameraMovement.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        //Camera Movement
+        Vector3 moveCamTo = transform.position - transform.forward * 2.0f + Vector3.up * 2.0f;
+        float bias = 0.2f;
+        Camera.main.transform.position = Camera.main.transform.position * bias + moveCamTo * (1.0f - bias);
+        Camera.main.transform.LookAt(transform.position + transform.forward * 20.0f);
+    }
+}
--- Assets/CALLI/Scripts/Plane_Movement.cs
+++ Assets/CALLI/Scripts/Plane_Movement.cs
@@ -40,13 +40,12 @@ public class Plane_Movement : MonoBehaviour
 
     void FixedUpdate()
     {
-
+        //Camera Movement
         Vector3 moveCamTo = transform.position - transform.forward * 2.0f + Vector3.up * 2.0f;
-        float bias = 0.96f;
+        float bias = 0.2f;
         Camera.main.transform.position = Camera.main.transform.position * bias + moveCamTo * (1.0f - bias);
         Camera.main.transform.LookAt(transform.position + transform.forward * 20.0f);
 
-        //Movement();
 
         // Vorwärsbewegung 
         transform.position += transform.forward * verticalMovement * speed * Time.deltaTime;
