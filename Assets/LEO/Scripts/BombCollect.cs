a43fe1607cc46e5754c52471fa3c01f8d8f85764
LeonieStrasser <leonie.strasser@yahoo.com>
2022-11-01 21:47:38 +0100

    Booster
    Added Booster

--- Assets/LEO/Scripts/BombCollect.cs
+++ Assets/LEO/Scripts/BombCollect.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class BombCollect : MonoBehaviour
 {
     BombTimer playerBomb;
+    CollectableBombTimer collectTimer;
 
     private void Awake()
     {
         playerBomb = FindObjectOfType<BombTimer>();
+        collectTimer = FindObjectOfType<CollectableBombTimer>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
+            collectTimer.ResetTimer();
             playerBomb.PickUpBomb();
             Destroy(this.gameObject);
         }
