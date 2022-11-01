efbe6f8534001c51d8a24719c280cf6a64cfc8a4
Deborah Mayer <deborahmayer02@gmail.com>
2022-11-01 16:58:03 +0100

    vertical city implemented (Base) ; new test scene

--- Assets/CALLI/Scripts/CameraMovement.cs
+++ Assets/CALLI/Scripts/CameraMovement.cs
@@ -8,7 +8,7 @@ public class CameraMovement : MonoBehaviour
     {
         //Camera Movement
         Vector3 moveCamTo = transform.position - transform.forward * 2.0f + Vector3.up * 2.0f;
-        float bias = 0.82f;
+        float bias = 0.84f;
         Camera.main.transform.position = Camera.main.transform.position * bias + moveCamTo * (1.0f - bias);
         Camera.main.transform.LookAt(transform.position + transform.forward * 20.0f);
     }
