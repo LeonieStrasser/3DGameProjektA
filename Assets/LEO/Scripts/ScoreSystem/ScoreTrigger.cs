ae3b6ca8358b46f77f99b7efb802f8ad3331a6b9
Leonie Straßer <leonie.strasser@yahoo.de>
2022-11-16 21:51:45 +0100

    fix

--- Assets/LEO/Scripts/ScoreSystem/ScoreTrigger.cs
+++ Assets/LEO/Scripts/ScoreSystem/ScoreTrigger.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [RequireComponent(typeof(Collider))]
 public class ScoreTrigger : MonoBehaviour
 {
-    [SerializeField] [Tooltip("Anzahl an Punkten die bei normalem durchfliegen auf den Score gerechnet werden")] int points = 5;
+    [SerializeField] [Tooltip("Anzahl an Punkten die bei normalem durchfliegen auf den Score gerechnet werden")] int ScorePoints = 5;
     [SerializeField] [Tooltip("Anzahl an Punkten die bei durchfliegen auf die Resource gerechnet werden")] int resourcePoints = 20;
 
     WhingMovement01 myPlayer;
@@ -24,7 +24,7 @@ public class ScoreTrigger : MonoBehaviour
     {
         if (other.tag == "Player" && ScoreSystem.Instance != null)
         {
-            ScoreSystem.Instance.AddScore(points);
+            ScoreSystem.Instance.AddScore(ScorePoints);
         }
         else
             Debug.LogWarning("Es fehlt ein ScoreSystem-Script in der Szene ihr Volldeppen!");
