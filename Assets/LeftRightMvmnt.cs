2587ebb58945d531cd4c54870fd10ecc8b831396
Lottifn <s0581953@htw-berlin.de>
2022-11-21 17:41:18 +0100

    Lotti&JoshTestlevel

--- Assets/LeftRightMvmnt.cs
+++ Assets/LeftRightMvmnt.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class LeftRightMvmnt : MonoBehaviour
 {
-    public float range = 1.5f;  // Amount to move left and right from the start point
+    public float Xrange = 0f;  // Amount to move left and right from the start point
+    public float Yrange = 0f;
+    public float Zrange = 0f;
     public float speed = 2.0f; 
     private Vector3 startPos;
+    private Vector3 v;
+
     
     
  
@@ -17,8 +21,11 @@ public class LeftRightMvmnt : MonoBehaviour
      
     void Update () 
     {
-        Vector3 v = startPos;
-        v.z += range * Mathf.Sin (Time.time * speed);
+        v = startPos;
+        v.x += Xrange * Mathf.Sin (Time.time * speed);
+        v.y += Yrange * Mathf.Sin (Time.time * speed);
+        v.z += Zrange * Mathf.Sin (Time.time * speed);
         transform.position = v;
+
     }
 }
