dccdf75c0dd9d02c8b4759e45e4c6ef9f2cef778
Leonie Straßer <leonie.strasser@yahoo.de>
2022-11-21 20:31:35 +0100

    VFX werden gespawnt beim nah ranfliegen

---
+++ Assets/LEO/Scripts/Environment/DistanceTracker.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DistanceTracker : MonoBehaviour
+{
+    [SerializeField] float detectionRadius;
+    [SerializeField] GameObject spawnVFX;
+    [SerializeField] [Range(0, 0.5f)] float spawnDelay;
+    [SerializeField] SphereCollider distanceTrigger;
+
+    bool spawnTimer;
+
+    private void Start()
+    {
+        distanceTrigger.radius = detectionRadius;
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (!other.isTrigger && other.tag != "Player")
+        {
+            Vector3 closestPoint = other.ClosestPoint(distanceTrigger.gameObject.transform.position);
+            if (Vector3.Distance(distanceTrigger.transform.position, closestPoint) > 0.001)
+            {
+                StartCoroutine(vfxSpawnTimer(closestPoint));
+            }else
+            {
+                Debug.LogWarning("Tryed to spawn a Distance-VFX on the player! Da war wahrscheinlich ein Mesh Collider schuld! Objektname: " + other.gameObject.name);
+            }
+        }
+
+
+
+    }
+
+    IEnumerator vfxSpawnTimer(Vector3 spawnPosition)
+    {
+        spawnTimer = true;
+        Instantiate(spawnVFX, spawnPosition, Quaternion.identity);
+        yield return new WaitForSeconds(spawnDelay);
+        spawnTimer = false;
+
+    }
+}
