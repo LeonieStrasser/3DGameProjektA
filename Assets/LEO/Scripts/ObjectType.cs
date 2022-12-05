011d568d8fa723f90fd2149290b6032297709cd4
Leonie Straßer <leonie.strasser@yahoo.de>
2022-12-05 16:40:20 +0100

    Aktivierbare Objekte

---
+++ Assets/LEO/Scripts/ObjectType.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectType : MonoBehaviour
+{
+    public  enum type
+    {
+        zahnradKlein,
+        zahnradGroß,
+        drehKürbis
+    }
+}
