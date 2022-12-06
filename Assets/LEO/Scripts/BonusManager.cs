1c2decaf10c8b2e2fc3fd459f216361d4e478fee
Leonie Straßer <leonie.strasser@yahoo.de>
2022-12-06 14:31:47 +0100

    Punktemultiplikator für bestimmte Zeit + FuelAdd

--- Assets/LEO/Scripts/BonusManager.cs
+++ Assets/LEO/Scripts/BonusManager.cs
@@ -5,11 +5,42 @@ using System;
 
 public class BonusManager : MonoBehaviour
 {
-    public event Action<ObjectType.type> OnActivateMoving;
+    [SerializeField] ObjectType.type typeToActivate;
+    [SerializeField] float addProcent;
+    [SerializeField] float effectTime;
+    [SerializeField] float multiplyer;
 
-    
-  public void ActivateMovingObject(ObjectType.type typeToActivate)
+    WhingMovement01 myPlayer;
+
+    public event Action<ObjectType.type> OnActivateMoving;
+    public event Action<float> OnTimeEffectStart;
+    public event Action<float> OnTimeEffectEnd;
+
+    private void Awake()
     {
-        OnActivateMoving?.Invoke(typeToActivate);
+        myPlayer = FindObjectOfType<WhingMovement01>();
+    }
+
+    public void ActivateMovingObject()//(ObjectType.type typeToActivate)
+    {
+        OnActivateMoving?.Invoke(this.typeToActivate);
+    }
+
+    public void AddFuelVolume()//(float addProcent)
+    {
+        myPlayer.AddMaxRecourcePoints(this.addProcent);
+    }
+
+    public void StartPointMultiplyTime()// (float effectTime, float multiplyer)
+    {
+        OnTimeEffectStart?.Invoke(this.multiplyer);
+        StartCoroutine(TimeEffect(this.effectTime));
+    }
+
+
+    IEnumerator TimeEffect(float effectTime)
+    {
+        yield return new WaitForSeconds(effectTime);
+        OnTimeEffectEnd?.Invoke(1); // damit der Multiplyer wieder 1 ist nach Ablauf der Zeit
     }
 }
