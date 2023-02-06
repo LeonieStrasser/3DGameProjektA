c5d29ffa750270f831c813e64cdf772ae30dc14f
Lottifn <s0581953@htw-berlin.de>
2023-02-06 14:02:07 +0100

    Animations

--- Assets/TutorialScript.cs
+++ Assets/TutorialScript.cs
@@ -5,136 +5,29 @@ using TMPro;
 
 public class TutorialScript : MonoBehaviour
 {
-    [Header("Slides")]
-    public GameObject firstSlide;
-    public GameObject secondSlide;
-    public GameObject thirdSlide;
-    public GameObject fourthSlide;
-
-    [Header("Buttons")]
-    public GameObject rbFirstSlide;
-    public GameObject lbSecondSlide;
-    public GameObject rbSecondSlide;
-    public GameObject lbThirdSlide;
-    public GameObject rbThirdSlide;
-    public GameObject lbFourthSlide;
-
      [Header("Animator")]
      public Animator anim;
-    private void Start() 
-    {
-        anim.SetBool("FirstSlide", true);
-    }
 
-    //METHODS
-    // We start at Slide #1
-    // Change from Slide #1 to #2
-   public void ChangeToSecondSlide()
-   {    
+     public void FirstSlideAnim()
+     {
+        anim.SetBool("FirstSlide", true);
+     }
+
+     public void SecondSlideAnim()
+     {    
         anim.SetBool("SecondSlide", true);
         anim.SetBool("FirstSlide", false);
         anim.SetBool("ThirdSlide", false);
-        
-        //SET ACTIVE
-        //SLIDES
-        secondSlide.SetActive(true);
-        
-        //BUTTONS
-        lbSecondSlide.SetActive(true);
-        rbSecondSlide.SetActive(true);
+     }
 
-
-        //SET FALSE
-        //SLIDES
-        firstSlide.SetActive(false);
-        thirdSlide.SetActive(false);
-        fourthSlide.SetActive(false);
-
-        //BUTTONS
-        rbFirstSlide.SetActive(false);
-        lbThirdSlide.SetActive(false);
-        rbThirdSlide.SetActive(false);
-        lbFourthSlide.SetActive(false);  
-   }
-
-   // Go back from Slide #2 to #1
-    public void ChangeToFirstSlide()
-   {
-        anim.SetBool("FirstSlide", true);
-        anim.SetBool("SecondSlide", false);
-        anim.SetBool("ThirdSlide", false);
-        //SET ACTIVE
-        //SLIDES
-        firstSlide.SetActive(true);
-
-        //BUTTONS
-        rbFirstSlide.SetActive(true);
-        
-        
-        //SET FALSE
-        //SLIDES
-        secondSlide.SetActive(false);
-        thirdSlide.SetActive(false);
-        fourthSlide.SetActive(false);
-
-        //BUTTONS
-        lbSecondSlide.SetActive(false);
-        rbSecondSlide.SetActive(false);
-        lbThirdSlide.SetActive(false);
-        rbThirdSlide.SetActive(false);
-        lbFourthSlide.SetActive(false);
-   }
-
-    // Go from Slide #2 to #3
-   public void ChangeToThirdSlide()
-   {
+     public void ThirdSlideAnim()
+     {
         anim.SetBool("ThirdSlide", true);
         anim.SetBool("SecondSlide", false);
-        //SET ACTIVE
-        //SLIDES
-        thirdSlide.SetActive(true);
+     }
 
-        //BUTTONS
-        lbThirdSlide.SetActive(true);
-        rbThirdSlide.SetActive(true);
-        
-
-        //SET FALSE
-        //SLIDES
-        firstSlide.SetActive(false);
-        secondSlide.SetActive(false);
-        fourthSlide.SetActive(false);
-
-        //BUTTONS
-        rbFirstSlide.SetActive(false);
-        lbSecondSlide.SetActive(false);
-        rbSecondSlide.SetActive(false);
-        lbFourthSlide.SetActive(false);
-   }
-
-    // Go from Slide #3 to #4
-   public void ChangeToFourthSlide()
+   public void FourthSlideAnim()
    {
         anim.SetBool("ThirdSlide", false);
-        //SET ACTIVE
-        //SLIDES
-        fourthSlide.SetActive(true);
-
-        //BUTTONS
-        lbFourthSlide.SetActive(true);
-
-
-        //SET FALSE
-        //SLIDES
-        firstSlide.SetActive(false);
-        secondSlide.SetActive(false);
-        thirdSlide.SetActive(false);
-
-        //BUTTONS
-        rbFirstSlide.SetActive(false);
-        lbSecondSlide.SetActive(false);
-        rbSecondSlide.SetActive(false);
-        lbThirdSlide.SetActive(false);
-        rbThirdSlide.SetActive(false);
    }
 }
